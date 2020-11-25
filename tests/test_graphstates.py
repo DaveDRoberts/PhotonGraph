2218194510328efdbbbd59ebc4c6526b2f89af66
David Roberts <daveroberts1994@live.co.uk>
2020-11-25 16:55:58 +0000

    Completed test_gs_utils

--- tests/test_graphstates.py
+++ tests/test_graphstates.py
@@ -94,3 +94,26 @@ def test_fusion():
     pass
 
 
+# @pytest.mark.parametrize("d, n, state_vector, exp_result", [
+#     (3, 3, state_vector_n3_d3_with_gp, {(1, 2): 2, (0, 1): 1, (0, 1, 2): 2}),
+#     (3, 3, state_vector_n3_d3_ru_not_graph, {}),
+#     (4, 4, state_vector_ghz_n4_d4, {}),
+#     (4, 3, state_vector_n3_d4, {(1, 2): 2, (0, 1): 1, (0, 1, 2): 3}),
+#     (2, 4, state_vector_n4_d2, {(2, 3): 1, (1, 3): 1, (1, 2): 1, (0, 1): 1})])
+# def test_graph_state_edges(d, n, state_vector, exp_result):
+#     assert graph_state_edges(d, n, state_vector) == exp_result
+#
+#
+# @pytest.mark.parametrize("d, n, edges, exp_result", [
+#     (4, 3, {(1, 2): 2, (0, 1): 1, (0, 1, 2): 3}, state_vector_n3_d4),
+#     (2, 4, {(2, 3): 1, (1, 3): 1, (1, 2): 1, (0, 1): 1}, state_vector_n4_d2)])
+# def test_state_vector_from_edges(d, n, edges, exp_result):
+#     """
+#     Checking if the produced dictionary is correct requires checking
+#     each item in turn
+#
+#     """
+#     test_result = state_vector_from_edges(d, n, edges)
+#
+#     assert np.all([np.isclose(test_result[state], exp_amp)
+#                    for state, exp_amp in exp_result.items()])
